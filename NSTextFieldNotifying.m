2658fce5a4977fe1e0949962697deff63204e3ec
Dale <DaleMyers19@gmailcom>
2012-08-17 22:11:56 +0100

    Initial commit

---
+++ NSTextFieldNotifying.m
@@ -0,0 +1,67 @@
+//
+//  NSTextFieldNotifying.m
+//
+//  Created by Dale Myers on 17/08/2012.
+
+/*
+    Copyright (c) 2012 Dale Myers
+
+    This software is provided 'as-is', without any express or implied
+    warranty. In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+*/
+
+#import "NSTextFieldNotifying.h"
+
+@implementation NSTextFieldNotifying
+
+@synthesize delegate;
+
+-(id)init{
+    self = [super init];
+    if(self){
+        [self registerForNotifications];
+    }
+    return self;
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if(self){
+        [self registerForNotifications];
+    }
+    return self;
+}
+
+-(id)initWithFrame:(NSRect)frameRect{
+    self = [super initWithFrame:frameRect];
+    if(self){
+        [self registerForNotifications];
+    }
+    return self;
+}
+
+-(void)registerForNotifications{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldDidChange:) name:NSControlTextDidChangeNotification object:self];
+}
+
+-(void)textFieldDidChange:(NSNotification *)notification{
+    [[self delegate] textFieldDidChange:self];
+}
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 NSTextFieldNotifying
 ====================
 
-An implementation of NSTextField and UITextField which have an extra method in the delegate to notify you whenever the text changes
+An extremely simple implementation of NSTextField which has an extra method in the delegate to notify you whenever the text changes
+
+The class is implemented as you usually would with a regular NSTextField
+programmatically, or if you use interface builder, change the custom class to
+NSTextFieldNotifying.
+
+Delegate
+--------
+
+The protocol extends the NSTextField protocol adding the method:
+
+`-(void)textFieldDidChange:(NSTextField *)textField;`
+
+Just implement your class as the delegate and remember to call
+
+`[textField setDelegate:self];`
+
+
+This class is simple, but it's saved me so much time and effort since I need
+this a lot and it means I don't have many methods scattered around since I get
+a notification with a reference to an actual NSTextField
+
